78e584892b826a139aeeed855c8689b31e6d8305
NiteshTiwari147 <64365692+NiteshTiwari147@users.noreply.github.com>
2022-09-26 19:02:51 +0530

    new files

--- script.js
+++ script.js
@@ -1,73 +1,12 @@
-const sidebar = document.getElementById('sidebar');
-const form = document.getElementById('form');
-const title = document.getElementById('title');
-const titleSearch = document.getElementById('titleSearch');
-const subTitle = document.getElementById('subTitle');
-const closeBtn = document.getElementById('closeBtn');
-const colorPallete = document.getElementById('clrPanel');
-const clrPaletteFilter = document.getElementById('clrPaletteFilter');
-const addCardBtn = document.getElementById('addCardBtn');
-const formColorButtons = document.querySelectorAll('.colorBtn');
-const filterColorButtons = document.querySelectorAll('.filterClrBtn');
 const cardContainer = document.getElementById('cardContainer');
-const barStatus = document.getElementById("barStatus");
-const cardCount = document.getElementById('cardCount');
-const searchError = document.getElementById('searchError');
-const formError = document.getElementById('formError');
-const addCardError = document.getElementById('addCardError');
-
-var data={};
-var selectedCards = [];
-
-// Fetch posts from API
-async function getColors() {
-    const res = await fetch(
-        `https://random-flat-colors.vercel.app/api/random?count=5`
-    );
-    data = await res.json();
-    const len = data.colors.length
-    if(len>0 && len<6) {
-        [...formColorButtons].map((btn,index) => {
-            btn.style.backgroundColor=data.colors[index];
-        });
-        [...filterColorButtons].map((btn,index) => {
-            btn.style.backgroundColor=data.colors[index];
-        })
-    }
-    return data;
-}
-
-// removing card
-function removeCards() {
-    while(cardContainer.firstChild) {
-        cardContainer.removeChild(cardContainer.lastChild);
-    }
-}
-
-// adding card
-function addCard(colorNumber,titleText,subTitleText) {
-    const card = document.createElement('div');
-    const cardTitle = document.createElement('h2');
-    const cardSubTitle = document.createElement('h4');
-    cardTitle.innerText = titleText;
-    cardSubTitle.innerText = subTitleText;
-    cardTitle.classList.add("cardTitle");
-    cardSubTitle.classList.add("cardSubTitle");
-    card.classList.add("card");
-    card.style.backgroundColor = colorNumber;
-    card.appendChild(cardTitle);
-    card.appendChild(cardSubTitle);
-    cardContainer.appendChild(card);
-}
-
-// update Progress bar
-function updateProgressBar() {
-    const totalCards = selectedCards.length;
-    const width = 6*totalCards;
-    barStatus.style.width = width + 'rem'; 
-    cardCount.innerText = `${totalCards}/5 Creatives`;
-}
-
+const watchList = document.getElementById('dropPoint');
+const watchListContainer = document.getElementById('watchListContent');
+const genreSelect = document.getElementById('genre');
+const titleSearch = document.getElementById('titleSearch');
+const ratingSelect = document.getElementById('ratings');
+var animeData = [];
+var draggingElementId = -1;
+const genres = new Set();
 
 function debounce(func, timeout = 300){
     let timer;
@@ -77,129 +16,251 @@ function debounce(func, timeout = 300){
     };
 }
 
-//searching card via text    
-function searchInput(){
+function searchAnime() {
     var filteredCard = [];
-    if(titleSearch.value.trim()==='') {
-        searchError.style.visibility= "hidden";
-        return;
-    }
     var searchFlag = false;
-    selectedCards.map(card => {
-        if(card.titleText.trim() === titleSearch.value.trim() || card.subTitleText.trim() === titleSearch.value.trim()) {
-            filteredCard.push(card);
+    animeData.map(item => {
+        if(item.title.toLowerCase().trim() === titleSearch.value.toLowerCase().trim()) {
+            filteredCard.push(item);
             searchFlag = true;
         }
     });
 
-    //if searched failed removing searched cards and retreiving old card
-    if(!searchFlag) {
-        searchError.style.color = "red";
-        searchError.style.visibility= "visible";
-        removeCards();
-        selectedCards.map(card => {
-            const {colorNumber,titleText,subTitleText} = card;
-            addCard(colorNumber,titleText,subTitleText);
-        });
-        return;
-    }
-
     if(filteredCard.length > 0) {
-        removeCards();
-        searchError.style.visibility= "hidden";
+        while(cardContainer.firstChild) {
+            cardContainer.removeChild(cardContainer.lastChild);
+        }
+        addCard(filteredCard);
+    } else {
+        titleSearch.style.color = 'red';
     }
-    filteredCard.map(card => {
-        const {colorNumber,titleText,subTitleText} = card;
-        addCard(colorNumber,titleText,subTitleText);
-    });
 }
-  
-const searchCard = debounce(() => searchInput());
 
-addCardBtn.addEventListener('click', e => {
-    if(selectedCards.length === 5) {
-        addCardBtn.classList.add('disabled');
-        addCardError.style.color = "red";
-        addCardError.style.visibility= "visible";
-        return;
-    }
-    sidebar.classList.toggle('collapsed');
-    addCardBtn.classList.toggle('disabled');
+
+const searchCard = debounce(() => {
+    titleSearch.style.color = 'black';
+    searchAnime()
 });
 
-closeBtn.addEventListener('click', e => {
-    sidebar.classList.toggle('collapsed');
-})
+function filterData(data) {
+    data.map((item,idx) => {
+        item.genres.map((genre,idx) => {
+            genres.add(genre.name);
+        })
+    });
+    var genreCount=0;
+    genres.forEach(function(value,idx) {
+        const option = document.createElement('option');
+        option.value = genreCount;
+        genreCount++;
+        option.innerText = value;
+        genreSelect.appendChild(option);
+      })
+}
 
-colorPallete.addEventListener('click', e => {
-    const selectedClr = document.querySelectorAll('.colorSelected');
-    [...selectedClr].map((btn,index) => {
-        if(btn != e.target) {
-            btn.classList.remove('colorSelected');
+function genreFilterCard(genreName) {
+    if(genreName != 'All genre') {
+        var filteredCards = [];
+        animeData.map((item,idx) => {
+            item.genres.map((genre,index) => {
+                if(genre.name === genreName) {
+                    filteredCards.push(item);
+                }
+            })
+        })
+        while(cardContainer.firstChild) {
+            cardContainer.removeChild(cardContainer.lastChild);
         }
-    })
-    e.target.classList.toggle('colorSelected');
-})
 
-clrPaletteFilter.addEventListener('click', e => {
-    let selectedClr = document.querySelectorAll('.colorSelected');
-    const {colors} = data;
-    [...selectedClr].map((btn,index) => {
-        if(btn != e.target) {
-            btn.classList.remove('colorSelected');
-        }
-    })
-    e.target.classList.toggle('colorSelected');
-    selectedClr = document.querySelectorAll('.colorSelected');
-    if( selectedClr.length === 0) {
-        removeCards();
-        selectedCards.map(card => {
-            const {colorNumber,titleText,subTitleText} = card;
-            addCard(colorNumber,titleText,subTitleText);
+        addCard(filteredCards);
+    } else {
+        addCard(animeData);
+    }
+}
+
+function ratingFilterCard(rating) {
+    if(rating != 0) {
+        var filteredCards = [];
+        animeData.map((item) => {
+            if(rating==1) {
+                if(item.score <= 5) {
+                    filteredCards.push(item)
+                }
+            } else if(rating==2) {
+                if(item.score > 5 && item.score <=8 ) {
+                    filteredCards.push(item)
+                }
+            } else if(rating==3) {
+                if(item.score > 8) {
+                    filteredCards.push(item)
+                }
+            }
         });
-        return;
-    }
-    const colorIndex = [...filterColorButtons].indexOf(selectedClr[0]);
-    const colorNumber = colors[colorIndex];
-    var filteredCard = [];
-    selectedCards.map(card => {
-        if(card.colorNumber === colorNumber) {
-            filteredCard.push(card);
+        while(cardContainer.firstChild) {
+            cardContainer.removeChild(cardContainer.lastChild);
         }
-    });
-    removeCards();
-    filteredCard.map(card => {
-        const {colorNumber,titleText,subTitleText} = card;
-        addCard(colorNumber,titleText,subTitleText);
-    })
-})
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const {colors} = data;
-    const selectedClr = document.querySelectorAll('.colorSelected');
-    const titleText = title.value;
-    const subTitleText = subTitle.value;
-    const colorIndex = [...formColorButtons].indexOf(selectedClr[0]);
-    const colorNumber = colors[colorIndex] || '';
-    if(colorNumber === '' || subTitleText === '' || titleText === '') {
-        formError.style.color = "red";
-        formError.style.visibility= "visible";
-        return;
+        addCard(filteredCards);
+    } else {
+        addCard(animeData);
     }
-    formError.style.visibility= "hidden";
-    selectedCards.push({
-        colorNumber,
-        titleText,
-        subTitleText
+}
+
+function addCard(data) {
+    data.map((item,idx) => {
+        const card = document.createElement('div');
+        card.draggable = true;
+        card.classList.add("card");
+        card.id = +idx;
+        const cardContent = document.createElement('div');
+        cardContent.classList.add("cardContent");
+        const thumbnailContainer = document.createElement('a');
+        thumbnailContainer.classList.add('animeThumbnail');
+        const thumbnail = document.createElement('img');
+        thumbnail.src = item.images.jpg.large_image_url;
+        const cardDetailContainer = document.createElement('div');
+        cardDetailContainer.classList.add('cardDetails');
+        const cardDetalRowOne = document.createElement('div');
+        const cardDetalRowTwo = document.createElement('div');
+        cardDetalRowOne.classList.add('cardRow');
+        cardDetalRowTwo.classList.add('cardRow');
+        const animeTitle = document.createElement('h2');
+        const watchListBtn = document.createElement('button');
+        watchListBtn.classList.add("btn");
+        watchListBtn.id = +idx;
+        watchListBtn.innerText = 'Add to watchlist';
+        watchListBtn.classList.add('btn');
+        animeTitle.innerText = item.title;
+        const genre = document.createElement('p');
+        let gens = '';
+        item.genres.map((genre,index) => {
+            if(index==0) {
+                gens  = gens.concat(`${genre.name}`);
+            } else {
+                gens  = gens.concat(`, ${genre.name}`);
+            }
+            
+        })
+        genre.innerText=`Genre: ${gens}`
+        const rating = document.createElement('p');
+        rating.innerText=`Rating : ${item.score}`;
+        cardDetalRowOne.appendChild(animeTitle);
+        cardDetalRowOne.appendChild(watchListBtn);
+        cardDetalRowTwo.appendChild(genre);
+        cardDetalRowTwo.appendChild(rating);
+        thumbnailContainer.appendChild(thumbnail)
+        cardDetailContainer.appendChild(cardDetalRowOne);
+        cardDetailContainer.appendChild(cardDetalRowTwo);
+        cardContent.appendChild(thumbnailContainer);
+        cardContent.appendChild(cardDetailContainer);
+        card.appendChild(cardContent);
+        cardContainer.appendChild(card);
     });
-    addCard(colorNumber,titleText,subTitleText);
-    updateProgressBar();
-    addCardBtn.classList.toggle('disabled');
-    title.value='';
-    subTitle.value=''
-    selectedClr[0].classList.toggle('colorSelected');
-    sidebar.classList.toggle('collapsed');
+    const cards = document.querySelectorAll('.card');
+    const addCardBtn = document.querySelectorAll('.btn');
+    for(const crd of cards) {
+        crd.addEventListener('dragstart',dragstart);
+    }
+    for(const btn of addCardBtn) {
+        btn.addEventListener('click', e => {
+            if(localStorage.getItem(e.target.id) == e.target.id) {
+                console.log("already there")
+            } else {
+                localStorage.setItem(e.target.id,e.target.id);
+                addWatchListCard(e.target.id)
+            }          
+        })
+    }
+    
+}
+
+function addWatchListCard(id) {
+    const watchListCard = document.createElement('div');
+    const watchListCardContent = document.createElement('div');
+    const thumbnailContainer = document.createElement('a');
+    const thumbnail = document.createElement('img');
+    const watchCardDetailContainer = document.createElement('div');
+    const animeTitle = document.createElement('h2');
+    const genre = document.createElement('p');
+    animeTitle.innerText = animeData[id].title;
+    let gens = '';
+    animeData[id].genres.map((genre,index) => {
+        if(index==0) {
+            gens  = gens.concat(`${genre.name}`);
+        } else {
+            gens  = gens.concat(`, ${genre.name}`);
+        } 
+    })
+    genre.innerText=`Genre: ${gens}`
+    watchCardDetailContainer.classList.add('watchCardDetails');
+    thumbnailContainer.classList.add('watchListThumbnail');
+    thumbnail.src = animeData[id].images.jpg.large_image_url;
+    watchListCard.classList.add("watchListCard");
+    watchListCardContent.classList.add('cardContent');
+    watchCardDetailContainer.appendChild(animeTitle);
+    watchCardDetailContainer.appendChild(genre);
+    thumbnailContainer.appendChild(thumbnail);
+    watchListCardContent.appendChild(thumbnailContainer);
+    watchListCardContent.appendChild(watchCardDetailContainer);
+    watchListCard.appendChild(watchListCardContent);
+    watchListContainer.appendChild(watchListCard);
+}
+
+function dragstart() {
+    draggingElementId=this.id;
+}
+
+function populateUI() {
+    const len = animeData.length;
+    for(var i=0;i<len;i++) {
+        if(localStorage.getItem(i)==i) {
+            addWatchListCard(i);
+        } 
+    }
+}
+
+watchListContainer.addEventListener('dragenter', dragEnter);
+
+ratingSelect.addEventListener('change', e => {
+    ratingFilterCard(e.target.value);
 });
 
-getColors();
+genreSelect.addEventListener('change', e => {
+    console.log(e.target.value);
+    [...genres].map((item,idx) => {
+        console.log(idx, e.target.value);
+        if(idx==e.target.value) {
+            genreFilterCard(item);
+        }
+    })
+    // ticketPrice = +e.target.value;
+    // setMovieData(e.target.selectedIndex, e.target.value);
+    // updateSelectedCount();
+});
+
+function dragEnter(e) {
+    e.preventDefault();
+    if(localStorage.getItem(draggingElementId) == draggingElementId) {
+        console.log("already there")
+    } else {
+        localStorage.setItem(draggingElementId,draggingElementId);
+        addWatchListCard(draggingElementId)
+    }
+   
+    console.log('Item entered', e.target);
+}
+
+async function getAnime() {
+    try {
+        const res = await fetch(`https://api.jikan.moe/v4/anime`)
+        const data = await res.json();
+        animeData=data.data;
+        addCard(animeData);
+        filterData(animeData);
+        populateUI();
+        return data;
+    } catch(err) {
+        console.log('Error occured while fetching data err: ',err);
+    }
+}
+
+getAnime();
